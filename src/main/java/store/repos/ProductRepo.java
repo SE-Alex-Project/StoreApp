9f7c1a4eb39fa27301094c2347171a6a798805cd
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-02-22 01:20:09 +0200

    top sales,total sales

--- src/main/java/store/repos/ProductRepo.java
+++ src/main/java/store/repos/ProductRepo.java
@@ -1,9 +1,31 @@
 package store.repos;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import store.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ProductRepo extends JpaRepository<Product,Integer> {
+    @Query(
+            nativeQuery = true,
+            value = """
+                    SELECT p.name ,p.price ,SUM(quantity) AS totalSales
+                    FROM Cart NATURAL JOIN cart_products NATURAL JOIN Product AS p
+                    WHERE buy_date >= DATE_ADD(NOW(),INTERVAL-90 DAY)
+                    GROUP BY p.id
+                    ORDER BY totalSales DESC LIMIT :count ;"""
+    )
+    Object topSales(@Param("count") Integer count);
 
+    @Query(
+            nativeQuery = true,
+            value = """
+                    SELECT p.name ,p.price ,SUM(quantity) AS totalSales
+                    FROM Cart NATURAL JOIN cart_products NATURAL JOIN Product AS p
+                    WHERE buy_date >= DATE_ADD(NOW(),INTERVAL-90 DAY)
+                    GROUP BY p.id
+                    ORDER BY totalSales DESC;"""
+    )
+    Object totalSales();
 }
